7496d6a296b05c03f91b26e061b97d6c37af9d61
SravaniMudiam555 <131254338+SravaniMudiam555@users.noreply.github.com>
2023-05-01 15:25:31 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,24 @@
+def newgit(repo)
+{
+  git "https://github.com/intelliqittrainings/{repo}.git"
+}
+
+def newmaven()
+{
+  sh "mvn package"
+}
+
+def newdeploy(jobname,ipaddress,cantext)
+{
+  sh "scp /var/lib/jenkins/workspace/{jobname}/webapp/target/webapp.war ubuntu@{ipaddress}:/var/lib/tomcat9/webapps/{context}.war"
+  
+}
+
+def newtest(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/{jobname}/testing.jar"
+  
+}
+
+
+  
